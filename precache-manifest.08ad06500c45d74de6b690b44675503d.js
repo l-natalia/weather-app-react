9f1bd10a171b6902f0fbeb68b92386e02645ef87
Natalia <nnati490@gmail.ccom>
2020-02-29 16:10:58 +0100

    Updates

---
+++ precache-manifest.08ad06500c45d74de6b690b44675503d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a2ec4c957758811aebe46a1fae6602",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "f35bc654181f6cd9637a",
+    "url": "/weather-app-react/static/css/main.f3975c1b.chunk.css"
+  },
+  {
+    "revision": "aaca2991786e4c1dce80",
+    "url": "/weather-app-react/static/js/2.68b54a6d.chunk.js"
+  },
+  {
+    "revision": "f35bc654181f6cd9637a",
+    "url": "/weather-app-react/static/js/main.4ff80010.chunk.js"
+  },
+  {
+    "revision": "3597222d31a75270aaa1",
+    "url": "/weather-app-react/static/js/runtime~main.c82ea8c9.js"
+  },
+  {
+    "revision": "f55bf052e99fefa1253154c551f937fc",
+    "url": "/weather-app-react/static/media/bg.f55bf052.jpg"
+  },
+  {
+    "revision": "405225a4a2c62a8247e8a520185ba93b",
+    "url": "/weather-app-react/static/media/pressure.405225a4.png"
+  },
+  {
+    "revision": "dbec09750cc984d6bffc56d6ce5e8129",
+    "url": "/weather-app-react/static/media/wind.dbec0975.png"
+  }
+]);
